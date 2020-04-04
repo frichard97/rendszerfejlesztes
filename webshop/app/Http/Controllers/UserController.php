23969ac4516b54ea6993e5fd6a1a9de1b8966179
frichard97 <foldesi.richard97@gmail.com>
2020-04-04 18:59:56 +0200

    fix

--- webshop/app/Http/Controllers/UserController.php
+++ webshop/app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function make_profile_view(){
-        return view('profile/create_profile_view');
+        return view('profile/make_profile_view');
     }
     public function profile_view(){
         return view('profile/profile_view');
