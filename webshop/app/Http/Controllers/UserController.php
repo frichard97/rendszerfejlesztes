df5ac35bbde66e15a7539d5ae0f991ad726054ec
frichard97 <foldesi.richard97@gmail.com>
2020-04-07 17:03:33 +0200

    added session example to view, and new_password validating and database update

--- webshop/app/Http/Controllers/UserController.php
+++ webshop/app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Profile;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 class UserController extends Controller
 {
     public function make_profile_view(){
@@ -42,7 +44,32 @@ class UserController extends Controller
         }
     }
     public function new_password(Request $request){
-        return "OK";
+        $validator = Validator::make($request->all(),[
+           'current_password' => 'required',
+           'new_password' => 'required',
+           'new_password_again' => ['required','same:new_password'],
+        ]);
+        if($validator->fails()){
+            return back()->withErrors($validator);
+        }
+        else
+        {
+            if(Hash::check($request['current_password'], Auth::user()->password)){
+                if($request['current_password'] == $request['new_password'])
+                {
+                    Session::flash('failed', 'Nem egyezhet a régi jelszó az új jelszóval');
+                    return back();
+                }
+                $user = Auth::user();
+                $user->password = Hash::make($request['new_password']);
+                $user->save();
+                Session::flash('success', 'Sikeres jelszó változtatás!');
+                return back();
+            }
+            else{
+                return back()->withErrors(['current_password' => "Rossz jelszó"]);
+            }
+        }
     }
     public function new_address(Request $request){
 
