c939d128c274e66f9b56c743e9c0bd14f0e837f7
uzsakj <feszoj18@gmail.com>
2020-04-07 20:17:44 +0200

    modified add_product form to match requirement

--- webshop/resources/views/product/make_product_view.blade.php
+++ webshop/resources/views/product/make_product_view.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js" defer></script>
+<script src="{{asset('js/ddm.js')}} " defer></script>
+
+@endpush
+@push('styles')
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+@endpush
 @section('content')
+
 <div class="card">
     <div class="card-body">
         <h2 class="card-title">Termék Hozzáadása</h2>
@@ -35,13 +45,17 @@
             <div class="form-group row">
                 <label for="product_category" class="col-4 col-form-label">Termék kategória</label>
                 <div class="col-4">
-                    <select class="form-control" id="product_category">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
+                        <select id="categories" class="form-control js-states js-example-basic-multiple" name="categories[]" multiple="multiple">
+                            <option value="1">Kategoria1</option>
+                            <option value="2">Kategoria2</option>
+                            <option value="3">Kategoria3</option>
+                            <option value="4">Kategoria4</option>
+                            <option value="5">Kategoria5</option>
+                            <option value="6">Kategoria6</option>
+                            <option value="7">Kategoria7</option>
+                            <option value="8">Kategoria8</option>
+                            <option value="9">Kategoria9</option>
+                        </select>
                 </div>
             </div>
             <div class="form-group row">
