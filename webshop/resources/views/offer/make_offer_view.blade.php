dc401fb15f59a087ab47fce9fe2ee786ca888b7a
uzsakj <feszoj18@gmail.com>
2020-04-22 13:47:29 +0200

    fixed whitelist errors

--- webshop/resources/views/offer/make_offer_view.blade.php
+++ webshop/resources/views/offer/make_offer_view.blade.php
@@ -55,55 +55,7 @@
                         </ul>
                     </div>
 
-                    <script>
-                        // Create a "close" button and append it to each list item
-                        var myNodelist = document.getElementsByTagName("LI");
-                        var i;
-                        for (i = 0; i < myNodelist.length; i++) {
-                            var span = document.createElement("SPAN");
-                            var txt = document.createTextNode("\u00D7");
-                            span.className = "close";
-                            span.appendChild(txt);
-                            myNodelist[i].appendChild(span);
-                        }
-
-                        // Click on a close button to hide the current list item
-                        var close = document.getElementsByClassName("close");
-                        var i;
-                        for (i = 0; i < close.length; i++) {
-                            close[i].onclick = function() {
-                                var li = document.createElement("li");
-                                li.remove(i)
-                            }
-                        }
-
-                        // Create a new list item when clicking on the "Add" button
-                        function newElement() {
-                            var li = document.createElement("li");
-                            var inputValue = document.getElementById("Input").value;
-                            var t = document.createTextNode(inputValue);
-                            li.appendChild(t);
-                            if (inputValue === '') {
-                                alert("Nem lehet üres!");
-                            } else {
-                                document.getElementById("whitelist").appendChild(li);
-                            }
-                            document.getElementById("Input").value = "";
-
-                            var span = document.createElement("SPAN");
-                            var txt = document.createTextNode("\u00D7");
-                            span.className = "close";
-                            span.appendChild(txt);
-                            li.appendChild(span);
-
-                            for (i = 0; i < close.length; i++) {
-                                close[i].onclick = function() {
-                                    var div = this.parentElement;
-                                    div.style.display = "none";
-                                }
-                            }
-                        }
-                    </script>
+                    
 
                 </div>
 
